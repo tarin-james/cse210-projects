126c9bac4e329668cca0bc35156fbdd6b4860306
Tarin James <122317277+tarin-james@users.noreply.github.com>
2023-09-16 11:25:02 -0600

    Learning Activity

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userGrade = Console.ReadLine();
+        int number = int.Parse(userGrade);
+
+        string letter;
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number < 90 && number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number < 80 && number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number < 70 && number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your Grade is a {letter}");
+        
+        if (letter == "D"|| letter == "F")
+        {
+            Console.WriteLine("You did not pass the class, study harder");
+        }
+        else
+        {
+            Console.WriteLine("Congrats! You passed the class");
+        }
+
     }
 }
